55806aeaa28dc20b0e2e67f37a3b87adaf1b50fc
Tim Drewnick <tim.drewnick@gmail.com>
2024-09-20 20:19:36 +0200

    add env variable input

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
 mod input_generation;
+mod merge_sort;
 
+use std::env;
 use input_generation::gen_input;
+use crate::merge_sort::merge_sort;
 
 fn main() {
-    println!("{:?}", gen_input(16));
+    let args: Vec<String> = env::args().collect();
+
+    let num_processors: usize = usize::from(&args[1]);
+    let input_size: usize = usize::from(&args[2]);
+
+    let input = gen_input(input_size);
+
+    merge_sort(num_processors, input);
 }
---
+++ src/merge_sort.rs
@@ -0,0 +1,4 @@
+pub fn merge_sort(num_processors: usize, input: Vec<i64>) -> Vec<i64> {
+
+    input
+}
