cb44a93608d07cd5583c9da73b4612c4df8a6622
Tim Drewnick <tim.drewnick@gmail.com>
2024-09-21 16:23:32 +0200

    basic functional merge sort

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,35 @@ mod input_generation;
 mod merge_sort;
 
 use std::env;
+use std::time::Instant;
 use input_generation::gen_input;
-use crate::merge_sort::merge_sort;
+use crate::merge_sort::par_merge_sort;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let num_processors: usize = usize::from(&args[1]);
-    let input_size: usize = usize::from(&args[2]);
+    let input_size: usize = args[1].parse().unwrap();
 
-    let input = gen_input(input_size);
+    let mut input: Vec<i64> = gen_input(input_size);
+    let mut scratch: Vec<i64> = input.clone();
 
-    merge_sort(num_processors, input);
+    println!("Unsorted: {:?}", input);
+    
+    let now = Instant::now();
+    
+    par_merge_sort(&mut input, &mut scratch);
+    
+    let elapsed = now.elapsed();
+    println!("Time: {:.3?}", elapsed);
+    println!("Sorted input: {:?}", is_sorted(input));
+}
+
+
+fn is_sorted(vec: Vec<i64>) -> bool {
+    for i in 0..vec.len() - 1{
+        if vec[i] > vec[i + 1] {
+            return false
+        }
+    }
+    true
 }
--- src/merge_sort.rs
+++ src/merge_sort.rs
@@ -1,4 +1,49 @@
-pub fn merge_sort(num_processors: usize, input: Vec<i64>) -> Vec<i64> {
+pub fn par_merge_sort<'input>(input: &'input mut [i64], scratch: &'input mut [i64]) {
+    let len = input.len();
+    if len == 1 {
+        return;
+    }
+    let mid = len / 2;
+    
+    let (left_input, right_input) = input.split_at_mut(mid);
+    let (left_scratch, right_scratch) = scratch.split_at_mut(mid);
+    
+    std::thread::scope(|scope| {
+        scope.spawn(|| par_merge_sort(left_input, left_scratch));
+        scope.spawn(|| par_merge_sort(right_input, right_scratch));
+    });
+    
+    
+    // merge in sequence
+    let left_len = mid;
+    let right_len = len - mid;
+    let (mut i, mut j, mut k) = (0, 0, 0);
+    
+    while i < left_len && j < right_len {
+        if left_scratch[i] < right_scratch[j] {
+            input[k] = left_scratch[i];
+            i = i + 1;
+        } else {
+            input[k] = right_scratch[j];
+            j = j + 1;
+        }
+        k = k + 1;
+    }
+    
+    while i < left_len {
+        input[k] = left_scratch[i];
+        k = k + 1;
+        i = i + 1;
+    }
 
-    input
+    while j < right_len {
+        input[k] = right_scratch[j];
+        k = k + 1;
+        j = j + 1;
+    }
+    
+    // might be a big slowdown -> try to find a more efficient solution or return input and scratch reversed like in pseudocode
+    for l in 0..len {
+        scratch[l] = input[l]
+    }
 }
